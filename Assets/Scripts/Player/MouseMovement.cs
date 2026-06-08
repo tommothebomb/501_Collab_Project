c37203800d8528515c5705eb8ef474c3d643fde2
LooneyTrooper66 <161621757+LooneyTrooper66@users.noreply.github.com>
2026-06-08 19:32:04 +0100

    player movement
    player movement and camera movement added all with new input system, player prefab has been made

--- Assets/Scripts/Player/MouseMovement.cs
+++ Assets/Scripts/Player/MouseMovement.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    // Libby script
-    [SerializeField] float mouseSense = 1f;
+    // Libby script \\
+    [SerializeField] float mouseSense = 15;
     [SerializeField] Transform body;
     float xRotation;
+    InputSystem_Actions inputActs;
 
+
+    private void OnEnable()
+    {
+        inputActs = new InputSystem_Actions();
+        inputActs.Player.Enable();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void OnDisable() => inputActs.Player.Disable();
 
     private void LateUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSense;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSense;
+        // new input system stuff for mouse
+        float mouseX = inputActs.Player.Look.ReadValue<Vector2>().x * mouseSense * Time.deltaTime;
+        float mouseY = inputActs.Player.Look.ReadValue<Vector2>().y * mouseSense * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
 using System;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Libby script
+    // Libby script \\
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float groundDrag = 5f;
+    [SerializeField] Transform groundCheckOrigin;
     Rigidbody rb;
+    Vector3 moveDirection;
+    LayerMask groundMask;
+    bool isGrounded;
+    InputSystem_Actions inputActs;
 
+    // player state machine
     public enum PlayerState
     {
         idle,
@@ -17,6 +27,17 @@ public class PlayerMovement : MonoBehaviour
     public static Action<PlayerState> OnPlayerStateChanged; // to be used if state needs to be updated and something something idk i cant figure this out
 
 
+    // input system and component gets
+    private void OnEnable()
+    {
+        inputActs = new InputSystem_Actions();
+        inputActs.Player.Enable();
+        rb = GetComponent<Rigidbody>();
+        groundMask = LayerMask.GetMask("Ground");
+    }
+    private void OnDisable() => inputActs.Player.Disable();
+
+    // state machine 
     public void SetMoveState(PlayerState playerState)
     {
         if (playerState == currentPlayerState) return;
@@ -44,8 +65,37 @@ public class PlayerMovement : MonoBehaviour
         currentPlayerState = playerState;
     }
 
+    // updates
     private void Update()
     {
-        // move things when press button
+        PlayerInput();
+        GroundCheck();
+    }
+    private void FixedUpdate() => MovePlayer();
+
+    // player input and movement handling 
+    void PlayerInput()
+    {
+        moveDirection = inputActs.Player.Move.ReadValue<Vector2>();
+        moveDirection = transform.forward * moveDirection.y + transform.right * moveDirection.x;
+    }
+    void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheckOrigin.position, 1, groundMask);
+
+        if (isGrounded) rb.linearDamping = groundDrag;
+        else rb.linearDamping = 0;
+    }
+    void MovePlayer()
+    {
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // limit velocity
+        Vector3 flatVal = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        if (flatVal.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVal.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
     }
 }
