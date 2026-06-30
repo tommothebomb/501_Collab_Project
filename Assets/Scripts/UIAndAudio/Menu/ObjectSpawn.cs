bb08d0751602d8f93cfc0c4e39005ff0c2065659
truckster29 <truckster26@gmail.com>
2026-06-30 09:11:43 +0100

    Further progress on MenuUI
    Changed some scripts for the objects falling behind the camera. Little bit of cleaning up and readability so others can easily implement and understand my script. Waiting for more prefabs to add to the list of falling objects.

--- Assets/Scripts/UIAndAudio/Menu/ObjectSpawn.cs
+++ Assets/Scripts/UIAndAudio/Menu/ObjectSpawn.cs
@@ -1,13 +1,21 @@
-using System;
 using UnityEngine;
 
 public class ObjectSpawn : MonoBehaviour
 {
+    [Header("// TIME VALUES //")]
     [SerializeField] float waitingTime = 5f;
     [SerializeField] float timer;
-    
-    [SerializeField] GameObject menuSlot;
 
+    [Header("// PREFAB COUNT //")]
+    [SerializeField] int prefabCount;
+
+    [Header("// PREFAB REFERENCES //")]
+    [SerializeField] GameObject[] prefab;
+
+    void Start()
+    {
+        timer = waitingTime;
+    }
     void Update()
     {
         timer += Time.deltaTime;
@@ -19,8 +27,16 @@ public class ObjectSpawn : MonoBehaviour
         }
     }
 
+    void FixedUpdate()
+    {
+    
+    }
+
     void SpawnObject()
     {
-        Instantiate(menuSlot, transform.position, Quaternion.identity);
+
+        int i = Random.Range(0, prefabCount);
+
+        Instantiate(prefab[i], transform.position, Quaternion.identity);
     }
 }
