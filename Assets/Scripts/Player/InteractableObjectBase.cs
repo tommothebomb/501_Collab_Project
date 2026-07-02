2f97a86e4850b1aea167db4df726ce141ffae8b8
tommothebomb <tomrhsollars@gmail.com>
2026-07-02 09:19:42 +0100

    what

--- Assets/Scripts/Player/InteractableObjectBase.cs
+++ Assets/Scripts/Player/InteractableObjectBase.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class InteractableObjectBase : MonoBehaviour
 {
     // Libby Script \\
-    [SerializeField] GameObject uiTooltipPrefab;
-    [SerializeField] Transform spawnPos;
+    [SerializeField] GameObject uiTooltipPrefab; // prefab for this is currently an elephant image
+    [SerializeField] Transform spawnPos; // set to pos of wherever you want the tooltip to show, usually just set as object transform
 
 
     void Start()
