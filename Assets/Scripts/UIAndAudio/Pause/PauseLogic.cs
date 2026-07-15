6cadeb54d055aaca1027408630467c40f31d0f62
truckster29 <truckster26@gmail.com>
2026-07-15 14:41:19 +0100

    Phone Apps ready for coding
    Phones apps are all added, just need to do the code for the text system

--- Assets/Scripts/UIAndAudio/Pause/PauseLogic.cs
+++ Assets/Scripts/UIAndAudio/Pause/PauseLogic.cs
@@ -6,6 +6,11 @@ public class PauseLogic : MonoBehaviour
     [SerializeField] GameObject returnToMenu;
     Animator phoneAnimator;
 
+    [SerializeField] GameObject appButtons;
+    [SerializeField] GameObject closeAppButton;
+    [SerializeField] GameObject settingsApp;
+    [SerializeField] GameObject messagesApp;
+
     void Start()
     {
         phoneAnimator = GameObject.FindGameObjectWithTag("PhoneUI").GetComponent<Animator>();
@@ -29,5 +34,32 @@ public class PauseLogic : MonoBehaviour
     public void UnPause()
     {
         phoneAnimator.SetBool("isPaused", false);
+        CloseApp();
+    }
+
+    public void OpenSettings()
+    {
+        OpenApp();
+        settingsApp.SetActive(true);
+    }
+
+    public void OpenMessages()
+    {
+        OpenApp();
+        messagesApp.SetActive(true);
+    }
+
+    void OpenApp()
+    {
+        appButtons.SetActive(false);
+        closeAppButton.SetActive(true);
+    }
+
+    public void CloseApp()
+    {
+        appButtons.SetActive(true);
+        closeAppButton.SetActive(false);
+        messagesApp.SetActive(false);
+        settingsApp.SetActive(false);
     }
 }
