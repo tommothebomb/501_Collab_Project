2f2144112f58c08214c203110bc2576fd9f49983
LooneyTrooper66 <161621757+LooneyTrooper66@users.noreply.github.com>
2026-07-16 16:13:13 +0100

    fixing bugs in npc behaviour

--- Assets/Scripts/NPCs/StandPointLogic.cs
+++ Assets/Scripts/NPCs/StandPointLogic.cs
@@ -4,4 +4,13 @@ using System.Collections.Generic;
 public class StandPointLogic : MonoBehaviour
 {
     public List<Transform> emptyStandPoints = new List<Transform>();
+
+
+    private void Awake()
+    {
+        foreach (Transform t in gameObject.transform)
+        {
+            emptyStandPoints.Add(t);
+        }
+    }
 }
