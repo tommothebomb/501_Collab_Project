a20f2c378a0e265d2d86f283a06037ce27032435
Nyabiii <167877215+DrownedInAPond@users.noreply.github.com>
2026-07-23 13:57:38 +0100

    meow

--- 501_Collab_Project_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ 501_Collab_Project_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,8 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_AMB_MIDCASINO = 4246019388U;
+        static const AkUniqueID PLAY_AMB_OUTSIDE = 121174090U;
         static const AkUniqueID PLAY_ATTRACT_FOURLEAFLUCKALT = 149938048U;
         static const AkUniqueID PLAY_ATTRACT_SNAKEEYES = 4097516578U;
         static const AkUniqueID PLAY_ATTRACT_WESTERN = 917334326U;
@@ -43,6 +45,27 @@ namespace AK
 
     } // namespace STATES
 
+    namespace SWITCHES
+    {
+        namespace FLOOR
+        {
+            static const AkUniqueID GROUP = 1088209313U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GENERIC = 4294388576U;
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace FLOOR
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -52,11 +75,20 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID INSIDE = 3553349781U;
         static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
         static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID OUTSIDE = 438105790U;
         static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID VOICELINES = 2292057722U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID INDOORAMBIENCE = 2564249364U;
+        static const AkUniqueID OUTDOOR_AMBIENCE = 1259851950U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
